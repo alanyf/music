832753c9bc67e4def67280149ebb19f8c8ca29b7
alanfyang <alanfyang@tencent.com>
2019-08-21 21:04:07 +0800

    ajax mock和线上数据根据环境切换

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,8 +1,9 @@
 import config from '../../config';
+const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
     protocal: 'http',
     host: 'http://localhost:3000/',
     localhost: 'http://localhost:' + config.dev.port,
     website: '/music/',
-    useMock: false
+    useMock: !isDev
 };
